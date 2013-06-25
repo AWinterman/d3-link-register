b29a2b4e5516e7cf9c377c099c7b64634f5547f2
AWinterman <andywinterman@gmail.com>
2013-06-25 15:45:16 +0100

    example is almost working. Currently breaking chrome

---
+++ example/main.js
@@ -0,0 +1,142 @@
+var d3 = require("d3")
+  , LinkRegister = require("../index.js")
+
+var width = 960
+  , height = 500
+  , CURSOR_WIDTH = 30
+  , DELETE_WIDTH = 10
+  , register = new LinkRegister(d3.layout.force(), null, [{}], 'idx')
+
+var fill = d3.scale.category20().domain(d3.range(20))
+
+var force = register.force
+    .size([width, height])
+    .linkDistance(30)
+    .charge(-60)
+    .on("tick", tick)
+
+register.init()
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .on("mousemove", mousemove)
+    .on("mousedown", mousedown)
+
+svg.append("rect")
+    .attr("width", width)
+    .attr("height", height)
+
+var node = svg.selectAll(".node")
+  , link = svg.selectAll(".link")
+    
+var cursor = svg.append("circle")
+    .attr("r", CURSOR_WIDTH)
+    .attr("transform", "translate(-100,-100)")
+    .attr("class", "cursor")
+
+d3.select(window).on("keydown", function(){
+  if (event.keyCode === 16) {
+    cursor.attr("r", DELETE_WIDTH)
+  }
+})
+
+d3.select(window).on("keyup", function(){
+  if (event.keyCode === 16) {
+    cursor.attr("r", CURSOR_WIDTH)
+  }
+})
+
+restart()
+
+function mousemove() {
+  cursor.attr("transform", "translate(" + d3.mouse(this) + ")")
+}
+
+function mousedown() {
+  if (!event.shiftKey) {
+    regular_mousedown.call(this)
+  }
+  else {
+
+    shift_mousedown.call(this)
+  }
+}
+
+function shift_mousedown() {
+  var point = d3.mouse(this)
+
+  register.nodes.forEach(function(target) {
+    var x = target.x - point[0]
+      , y = target.y - point[1] 
+    if (Math.sqrt(x*x + y*y) < DELETE_WIDTH) {
+      register.remove_node(target)
+    }
+  })
+
+  restart()
+}
+
+function regular_mousedown() {
+  var point = d3.mouse(this)
+    , node = {x: point[0], y: point[1]}
+
+  // nodes.push(node)
+  register.add_node(node)
+
+  // add links to any nearby nodes
+  register.nodes.forEach(function(target) {
+    var x = target.x - node.x,
+        y = target.y - node.y
+    if (Math.sqrt(x * x + y * y) < CURSOR_WIDTH) {
+      register.add_link({source: node, target: target})
+    }
+  })
+  restart()
+}
+
+function tick() {
+  link.attr("x1", function(d) { return d.source.x })
+      .attr("y1", function(d) { return d.source.y })
+      .attr("x2", function(d) { return d.target.x })
+      .attr("y2", function(d) { return d.target.y })
+
+  node.attr("transform", function(d){
+    return "translate(" + d.x + "," + d.y + ")"
+  })
+  //  .attr("cx", function(d){ return d.x})
+  //  .attr("cy", function(d){ return d.y})
+
+}
+
+function restart() {
+  var cn
+  link = link.data(register.links)
+
+  link.enter().insert("line", ".node")
+      .attr("class", "link")
+
+  link.exit().remove()
+
+  console.log(register.nodes.map(function(d){ return d.idx }))
+  node = node.data(register.nodes, function(d){ return d.idx })
+
+  node.exit().remove()
+
+  cn = node.enter().insert("g", ".cursor")
+      .attr("class", "node")
+
+  cn.append("circle")
+      .attr("r", 5)
+      .style("fill", function(d) { return fill(d.idx % 20) })
+
+  cn.append("text")
+    .attr("transform", "translate(10, 5)")
+    .text(function(d){ return d.idx })
+
+  cn.call(force.drag)
+
+  force.start()
+}
+
+
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // Class to handle link manipulations for D3 linked graphs.
-
 module.exports = LinkRegister
 
-function LinkRegister(links, nodes, index_attr){
-  this.links = links
-  this.nodes = nodes
-  this.index_attr = index_attr || 'idx'
+function LinkRegister(force, links, nodes, index_attr){
+  this.force = force
+  this.links = links || []
+  this.nodes = nodes || []
+  this.index_attr = index_attr || 'index'
   this.register = {}
+
+  this._circular_links = false
+  this._directed = false
 
   this.ensure_shape()
 }
+
 
 var cons = LinkRegister
   , proto = cons.prototype
 
 // if nodes don't have an index, assign them one. If you did not set the third
-// argument when you initialized the register, it defaults to "idx"
+// argument when you initialized the register, it defaults to "index"
 // Careful here, this will modify your node objects! 
 
 // links are nmaed based on their index_attr
@@ -23,85 +27,163 @@ proto.name = function(link){
   return link.source[this.index_attr] + "," + link.target[this.index_attr]
 }
 
+proto.circular_links = function(bool){
+  this._circular_links = bool || this._circular_links
+  var out = arguments.length ? this : this._circular_links 
+  return out
+}
+
+proto.directed = function(bool){
+  this._directed = bool || this._directed
+  var out = arguments.length ? this : this._directed 
+  return out
+}
+
 proto.index = function(link){
+  var source_match 
+    , target_match
+    , source_to_target
+    , target_to_source
   // find the element of the link array whose source[this.index_attr] and target[this.index_attr] attributes
   // match those of the argument
-  for (var i = 0, len = this.links.length; i < len; ++i){
-     source_match = link.source[this.index_attr] === this.links[i].source[this.index_attr]
-     target_match = link.target[this.index_attr] === this.links[i].target[this.index_attr]
-     if (source_match && target_match){
-       return i
-     }
-  }
-  return -1
+    for (var i = 0, len = this.links.length; i < len; ++i){
+       source_match = link.source[this.index_attr] === this.links[i].source[this.index_attr]
+       target_match = link.target[this.index_attr] === this.links[i].target[this.index_attr]
+       if (!this.directed()) {
+         source_to_target = link.source[this.index_attr] === this.links[i].target[this.index_attr]
+         target_to_source = link.target[this.index_attr] === this.links[i].source[this.index_attr]
+       }
+       if ((source_match && target_match) || (source_to_target && target_to_source) ) {
+         return i
+       }
+    }
+    return -1
 }
 
 proto.has = function(link){
-  return !!this.register[this.name(link)]
+  var name = this.name(link)
+  if (!this.circular_links()) {
+    if (link.target[this.index_attr] === link.source[this.index_attr]) {
+      return true
+    }
+  }
+  if (!this.directed()) {
+    return !!this.register[name] || !!this.register[name.split().reverse().join()]
+  } else {
+    return !!this.register[name] 
+  }
 }
   
 // make sure we don't already have the link, if we don't add it.
-proto.add = function(link){
-  if(!this.has(link)){
-    this.register[this.name(link)] = true
+proto.add_link = function(link) {
+  if(!this.has(link)) {
+    this.register[this.name(link)] = link
+    // the following two lines seem redundant
     this.links.push(link)
-    link.target.linked = true
-    link.source.linked = true
+    this.force.links(this.links)
     return true
   }
   return false
 }
 
-  // remove the attribute from the register
-proto.remove = function(link){
-  if(this.has(link)){
+proto.add_node = function(node) {
+  this.nodes.push(node)
+  // the following seem redundant
+  this.force.nodes(this.nodes)
+  this.ensure_shape()
+  return node
+}
 
+
+proto.remove_link = function(link) {
+  var reverse 
+  if (this.directed()) {
+    return remove_link.call(this, link)
+  }
+  reverse = {target: link.source, source: link.target}
+  return remove_link.call(this, link) || remove_link.call(this, reverse)
+}
+
+
+// remove the attribute from the register
+function remove_link(link) {
+  if(this.has(link)) {
     //note that links that were once in the register but are no longer now
     //reference the null object
     this.register[this.name(link)] = null
 
     // find the element in the links array and remove it
     this.links.splice(this.index(link), 1)
+    this.force.links(this.links)
     return true
   }
   // otherwise we don't need to do anything
   return false
 }
 
-proto.ready = function(d3_graph_force){
+proto.orphan_node = function(node) {
+  // for each link that ends or begins at the node, remove the link
+  var remove
+    , link
+  for (var name in this.register) {
+    // check to see if the i-th link has the node as a target or as a source
+    link = this.register[name]
+    if (link === null){
+      continue
+    }
+    remove = link.target[this.index_attr] === node[this.index_attr] ||
+                 link.source[this.index_attr] === node[this.index_attr]
+    if (remove) {
+      this.remove_link(link)
+    }
+  }
+}
+
+// orphan the node, then remove it from the nodes object
+proto.remove_node = function(node) {
+  var idx
+  this.orphan_node(node)
+  this.nodes.some(function(d, i){
+    idx = d[this.index_attr]
+    return d[this.index_attr] === node[this.index_attr]
+  }, this)
+  this.nodes.splice(idx, 1)
+}
+
+proto.init = function(){
   // A convenience function for d3
   // for easy interop with d3
-  d3_graph_layout.nodes(this.nodes)
-                 .links(this.links)
-  return d3_graph_layout
+  this.force.nodes(this.nodes)
+    .links(this.links)
+  return this.force 
 }  
-
 
 proto.ensure_shape = function(){
   // unindexed and range both hold indices
   var unindexed = []
     , range = []
+    , used = []
 
-  for(var i = 0, len = this.nodes.length; i < len; ++i){
-    range.push(i)
-    // check if the i-th node has an index attribute
-    if(this.nodes[i][this.index_attr] === undefined){
-      unindexed.push(i)
+  this.nodes.forEach(function(d, i){ 
+    var idx = d[this.index_attr]
+    if (idx !== undefined) {
+      used.push(idx)
     } else {
-      // then make sure the `i` is not in the range array.
-      if (range.indexOf(i) > -1){
-        //then we need to remove i from range
-        range.splice(i, 1)
-      }
-    } 
-  }
+      unindexed.push(i)
+    }
+    range.push(i)
+  }, this)
 
-  // for each unindex element, pop an element from range, call it `new_id`, and
-  // assign it to the unindexed element
-  for(var i = 0, len = unindexed.length; i < len; ++i){
-    var new_id = range.pop()
-    this.nodes[unindexed[i]][this.index_attr] = new_id
-  }
+  // now I want those elements in range that are not used
+  range = range.filter(function(d){
+    return used.indexOf(d) === -1
+  })
+
+  unindexed.forEach(function(d){
+    this.nodes[d][this.index_attr] = range.pop()
+  }, this)
+
 
   return this.nodes
 }
+
