2f9440e9eb8b602942b5589f78ad04878da7371d
AWinterman <andywinterman@gmail.com>
2013-06-26 15:11:27 +0100

    all tests pass, once again, as they are supposed to.

--- tests.js
+++ tests.js
@@ -4,6 +4,7 @@ var test = require("tape")
 var LinkRegister = require("./index")
   , assert = require("assert")
   , nodes = []
+  , force = {links: identity, nodes: identity}
 
 nodes.length = 20
 for (var i = 0, len = nodes.length; i < len; i = i +  2){
@@ -12,41 +13,30 @@ for (var i = 0, len = nodes.length; i < len; i = i +  2){
   //some dummy nodes
 }
 
+
 //initializing register object 
-var register = new LinkRegister([], nodes)
+var register = new LinkRegister(force, null, nodes, "idx")
 
-test("has nodes", function(t){
-  t.plan(1)
+test("has the right attributes", function(t){
+  t.plan(11)
   t.notDeepEqual(register.nodes, undefined)
-})
-test("has links", function(t){
-  t.plan(1)
   t.notDeepEqual(register.links, undefined)
-})
-test("has names", function(t){
-  t.plan(1)
   t.notDeepEqual(register.name, undefined)
-})
-test("has has", function(t){
-  t.plan(1)
   t.notDeepEqual(register.has, undefined)
-
-})
-test("has add", function(t){
-  t.plan(1)
-  t.notDeepEqual(register.add, undefined)
-
-})
-test("has ready", function(t){
-  t.plan(1)
-  t.notDeepEqual(register.ready, undefined)
+  t.notDeepEqual(register.add_link, undefined)
+  t.notDeepEqual(register.add_node, undefined)
+  t.notDeepEqual(register.remove_link, undefined)
+  t.notDeepEqual(register.remove_node, undefined )
+  t.notDeepEqual(register.orphan_node, undefined )
+  t.notDeepEqual(register.init, undefined)
+  t.notDeepEqual(register.ensure_shape, undefined)
 })
 
 test("nodes all have idx", function(t){
   t.plan(register.nodes.length)
   register.nodes.forEach(
     function(d, i){ 
-      t.notDeepEqual(d.idx, undefined, "node " + i + " has no idx attribute" )
+      t.notDeepEqual(d.idx, undefined, "node " + i + " has an idx attribute" )
     }
   )
 })
@@ -61,14 +51,14 @@ test("no repeated indices", function(t){
     return a
   }, [])
 
-  t.deepEqual(result.length, expected, "result had the wrong lenght")
+  t.deepEqual(result.length, expected, "result had right length")
 })
 
 test('register.index', function(t){
   t.plan(1)
-  var register = new LinkRegister([], nodes)
+  var register = new LinkRegister(force, null, nodes, "idx")
   var L = {'source': register.nodes[0], 'target': register.nodes[1]}
-  register.add(L)
+  register.add_link(L)
 
   var expected = 0
     , result = register.index(L)
@@ -83,14 +73,14 @@ test("can add links", function(t){
 test("does not duplicate existing links", function(t){
   t.plan(1)
 
-  var register = new LinkRegister([], nodes)
+  var register = new LinkRegister(force, null, nodes, "idx")
     , i = ~~ (Math.random() * nodes.length)
     , j = ~~ (Math.random() * nodes.length)
     , new_link = {source: register.nodes[i], target: register.nodes[j]}
 
-  register.add(new_link)
-  register.add(new_link)
-  register.add(new_link)
+  register.add_link(new_link)
+  register.add_link(new_link)
+  register.add_link(new_link)
 
   t.deepEqual(register.links.length, 1, "there were duplicates")
 
@@ -99,18 +89,20 @@ test("does not duplicate existing links", function(t){
 test("remove a link", function(t){
   t.plan(4)
   //remove a few links:
-  var register = new LinkRegister([], nodes)
+  var register = new LinkRegister(force, null, nodes, "idx")
   var n = 2
   make_links(register, 30)
   for (var i = 0; i < n; ++i){
     link_index = ~~ (Math.random() * register.links.length)
     var L = register.links[i]
-    register.remove(L)
+    register.remove_link(L)
     //now make sure the link is not in the register
     t.deepEqual(register.register[register.name(L)], null)
     t.deepEqual(register.links.indexOf(L), -1)
   }
 })
+
+function identity(a){ return a }
 
 function make_links(register, n){
   while (register.links.length < 30){
@@ -120,7 +112,7 @@ function make_links(register, n){
       , new_link
 
     new_link = {source: register.nodes[i], target: register.nodes[j]}
-    register.add(new_link)
+    register.add_link(new_link)
   }
   return register.links
 }
